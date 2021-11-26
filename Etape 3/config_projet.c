86d562c6129fd72ae40d14d419bdf85cde096849
loulou4418 <91460250+loulou4418@users.noreply.github.com>
2021-11-26 10:47:20 +0100

    sources etape3

---
+++ Etape 3/config_projet.c
@@ -0,0 +1,78 @@
+/*
+ * config_projet.c
+ *
+ * Created: 19/11/2021 09:23:12
+ *  Author: e182232d
+ */ 
+#include "conf_projet.h"
+
+void config_GCLK_TC6(void)
+{
+	Gclk *ptr_GCLK = GCLK;
+	
+	ptr_GCLK->CLKCTRL.reg = GCLK_CLKCTRL_NOWRTLOCK |
+							GCLK_CLKCTRL_CLKEN |
+							GCLK_CLKCTRL_GEN_GCLK0 |
+							GCLK_CLKCTRL_ID_TC6_TC7 ;
+							
+	ptr_GCLK->GENCTRL.reg = GCLK_GENCTRL_NORUNSTDBY |
+							GCLK_GENCTRL_NODIVSEL |
+							GCLK_GENCTRL_NOOE |
+							GCLK_GENCTRL_NOOOV |
+							GCLK_GENCTRL_NOIDC |
+							GCLK_GENCTRL_GENEN |
+							GCLK_GENCTRL_SRC_OSC8M |
+							GCLK_GENCTRL_ID(0);
+}
+
+void config_PM_TC6(void){
+	Pm *ptr_PM = PM;
+	
+	ptr_PM -> CPUSEL.reg = PM_CPUSEL_CPUDIV_DIV1;
+	ptr_PM -> APBCSEL.reg = PM_APBCSEL_APBCDIV_DIV1;
+	ptr_PM -> APBCMASK.reg |= PM_APBCMASK_TC6;
+}
+
+void config_PORT(void){
+	Port *ptr_port = PORT ;
+	
+	ptr_port -> Group[1].DIRSET.reg = PORT_PB02 | PORT_PB06;
+	ptr_port -> Group[1].PMUX[2/2].reg = PORT_PMUX_PMUXE_E;
+	ptr_port -> Group[1].PINCFG[2].reg = PORT_PINCFG_DRVSTR_NO |
+										  PORT_PINCFG_PULLEN_NO |
+										  PORT_PINCFG_INEN_NO |
+										  PORT_PINCFG_PMUXEN;
+}
+
+
+
+void config_TC6(void){
+	Tc *ptr_TC = TC6 ;
+	
+	ptr_TC -> COUNT16.CTRLA.reg = TC_CTRLA_PRESCSYNC_PRESC |
+								  TC_CTRLA_NORUNSTDBY |
+								  TC_CTRLA_PRESCALER_DIV1 |
+								  TC_CTRLA_WAVEGEN_MFRQ |
+								  TC_CTRLA_MODE_COUNT16 |
+								  TC_CTRLA_NOENABLE |
+								  TC_CTRLA_NOSWRST;
+	ptr_TC -> COUNT16.CTRLC.reg = TC_CTRLC_NOCPTEN0 |
+								  TC_CTRLC_NOCPTEN1 |
+								  TC_CTRLC_NOINVEN0 |
+								  TC_CTRLC_NOINVEN1;
+	ptr_TC -> COUNT16.CTRLBCLR.reg = TC_CTRLBCLR_CMD_NONE |
+									 TC_CTRLBCLR_ONESHOT |
+									 TC_CTRLBCLR_DIR;
+	ptr_TC -> COUNT16.CC[0].reg = 7999; //0X1387 (4999)_10
+	
+	ptr_TC -> COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE;								 
+								  
+}
+
+
+void configure_TC6_IT(void){
+	Tc *ptr_TC = TC6;
+
+	ptr_TC->COUNT16.INTENSET.reg = TC_INTFLAG_MC0;
+
+}
