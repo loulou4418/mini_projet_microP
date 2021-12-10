b6de0f4ad301ccd6f38343b186dce5df12d6f6c4
loulou4418 <91460250+loulou4418@users.noreply.github.com>
2021-12-10 11:08:27 +0100

    garabit et (pwm non fonctionnel)

--- Etape4/config_projet.c
+++ Etape4/config_projet.c
@@ -43,23 +43,33 @@ void config_GCLK_TC6(void)
 							GCLK_GENDIV_DIV(25);
 }
 
-void config_PM_TC6(void){
+void config_PM(void){
 	Pm *ptr_PM = PM;
 	
 	ptr_PM -> CPUSEL.reg = PM_CPUSEL_CPUDIV_DIV1;
 	ptr_PM -> APBCSEL.reg = PM_APBCSEL_APBCDIV_DIV1;
-	ptr_PM -> APBCMASK.reg |= PM_APBCMASK_TC6 | PM_APBCMASK_DAC;
+	ptr_PM -> APBCMASK.reg |= PM_APBCMASK_TC6 | PM_APBCMASK_TC7 | PM_APBCMASK_DAC;
 }
 
 void config_PORT(void){
 	Port *ptr_port = PORT ;
 	
-	ptr_port -> Group[1].DIRSET.reg = PORT_PB02 | PORT_PB06;
+	ptr_port -> Group[1].DIRSET.reg = PORT_PB02 | PORT_PB06 ; //  PORT_PB06 6/2 ????
 	ptr_port -> Group[1].PMUX[2/2].reg = PORT_PMUX_PMUXE_E;
 	ptr_port -> Group[1].PINCFG[2].reg = PORT_PINCFG_DRVSTR_NO |
 										  PORT_PINCFG_PULLEN_NO |
 										  PORT_PINCFG_INEN_NO |
 										  PORT_PINCFG_PMUXEN;
+	/* PWM */									  
+	ptr_port -> Group[1].DIRSET.reg =  PORT_PB01;
+	ptr_port -> Group[1].PMUX[1/2].reg = PORT_PMUX_PMUXO_E;
+	ptr_port -> Group[1].PINCFG[1].reg = PORT_PINCFG_DRVSTR_NO |
+											PORT_PINCFG_PULLEN_NO |
+											PORT_PINCFG_INEN_NO |
+											PORT_PINCFG_PMUXEN;
+	
+	
+	
 	/* DAC */								  
 	ptr_port -> Group[0].DIRSET.reg = PORT_PA02;
 	ptr_port -> Group[0].PMUX[2/2].reg = PORT_PMUX_PMUXE_B;
@@ -127,4 +137,30 @@ void config_DAC(void){
 	//while (ptr_DAC->STATUS.reg != 0);
 	ptr_DAC ->DATA.reg = 0;
 	
+}
+
+void config_PWM (void){
+		Tc *ptr_TC = TC7 ;
+		
+		ptr_TC -> COUNT16.CTRLA.reg = TC_CTRLA_PRESCSYNC_PRESC |
+										TC_CTRLA_NORUNSTDBY |
+										TC_CTRLA_PRESCALER_DIV1 |
+										TC_CTRLA_WAVEGEN_MPWM |
+										TC_CTRLA_MODE_COUNT16 |
+										TC_CTRLA_NOENABLE |
+										TC_CTRLA_NOSWRST;
+										
+		ptr_TC -> COUNT16.CTRLC.reg = TC_CTRLC_NOCPTEN0 |
+										TC_CTRLC_NOCPTEN1 |
+										TC_CTRLC_NOINVEN0 |
+										TC_CTRLC_NOINVEN1;
+										
+		ptr_TC -> COUNT16.CTRLBCLR.reg = TC_CTRLBCLR_CMD_NONE |
+											TC_CTRLBCLR_ONESHOT |
+											TC_CTRLBCLR_DIR;
+											
+		ptr_TC -> COUNT16.CC[0].reg = 800; // 10kHz
+		ptr_TC -> COUNT16.CC[1].reg = 400; // duty 50%
+		
+		ptr_TC -> COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE;
 }
---
+++ Etape4/main.c
@@ -0,0 +1,164 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * This is a bare minimum user application template.
+ *
+ * For documentation of the board, go \ref group_common_boards "here" for a link
+ * to the board-specific documentation.
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# Minimal main function that starts with a call to system_init()
+ * -# Basic usage of on-board LED and button
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+#include <asf.h>
+#include "conf_projet.h"
+
+void procedure_traitement_IT(void);
+uint16_t calcul_gabarit ();
+
+int main (void)
+{
+	system_init();
+	
+	config_GCLK_TC6();
+	config_PM();
+	config_PORT();
+	config_TC6();
+	
+	configure_TC6_IT();
+	
+	config_DAC();
+	
+	config_PWM ();
+
+	//Faire l'interruption
+	system_interrupt_enable(SYSTEM_INTERRUPT_MODULE_TC6);
+
+
+	/* Insert application code here, after the board has been initialized. */
+
+	/* This skeleton code simply sets the LED to the state of the button. */
+	while (1) {
+
+		/* Is button pressed? */
+		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
+			/* Yes, so turn LED on. */
+			port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		} else {
+			/* No, so turn LED off. */
+			port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		}
+	}
+} 
+
+void procedure_traitement_IT(void){
+	Port *ptr_Port = PORT;
+
+	ptr_Port->Group[1].OUTTGL.reg = PORT_PB06;
+	
+
+	
+
+}
+
+void TC6_Handler(void){
+	Tc *ptr_TC = TC6;
+
+	procedure_traitement_IT();
+	
+	Dac * ptr_DAC = DAC;
+	ptr_DAC -> DATA.reg = calcul_gabarit ();
+
+	ptr_TC->COUNT16.INTFLAG.reg = TC_INTFLAG_MC0;
+}
+
+uint16_t calcul_gabarit (){
+		typedef enum state_t
+		{
+			WAIT,
+			RISE,
+			CTE,
+			FALL
+		} state_t;
+
+		static state_t state = WAIT;
+		static uint16_t time = 0;
+		static uint16_t DACValue = 0;
+
+		switch (state)
+		{
+			case WAIT:
+			time++;
+			if (time >= 64)
+			{
+				state = RISE;
+				time = 0;
+			}
+			break;
+
+			case RISE:
+			time ++;
+			if (DACValue < (1024 - 8)){
+				DACValue += 8;
+			}
+			if (time >= 128)
+			{
+				state = CTE;
+				DACValue = 1023;
+				time = 0;
+			}
+			break;
+
+			case CTE:
+			time++;
+			if (time >= 512)
+			{
+				state = FALL;
+				time = 0;
+			}
+			break;
+
+			case FALL:
+			time++;
+			if(DACValue > (0 + 8)){
+				DACValue -= 8;
+			}
+			/* time : 1023/8 = 128 */
+			if (time >= 128)
+			{
+				state = WAIT;
+				DACValue = 0;
+				time = 0;
+			}
+			break;
+
+			/* Etat defaut = erreur raz variables */
+			default:
+			state = WAIT;
+			time = 0;
+			DACValue = 0;
+			break;
+		}
+	return DACValue;
+}
